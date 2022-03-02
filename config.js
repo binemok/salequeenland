35edf42caaa57de99ea2a981aa73c0d8287eb9ca
binemok <94422811+binemok@users.noreply.github.com>
2022-03-02 16:50:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ var firefly_count = 20;
 
 staff("Worthier Wind", "86fa328f-a99a-4a17-8250-5d333dd327fe", "Owner");
 staff("Binemok", "86fa328f-a99a-4a17-8250-5d333dd327fe", "Developer");
-staff("OJWMine", "86fa328f-a99a-4a17-8250-5d333dd327fe", "Admin");
+staff("Manhboom", "86fa328f-a99a-4a17-8250-5d333dd327fe", "Admin");
 
 vote("Planet-Minecraft", "https://i.postimg.cc/9fmyrRkJ/Transparent-Planet-Minecraft-Logo.png", "https://planetminecraft.com");
 vote("Minecraft Servers Org", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraftservers.org/");
