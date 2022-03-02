a9fbc688cf7a74805a49983f083870c341cb4225
binemok <94422811+binemok@users.noreply.github.com>
2022-03-02 14:08:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,15 @@
  * License - https://olliejw.me/tos
  */
 
-var server_name = "YourServer";
-var server_ip = "hypixel.net";
-var server_port = 25565;
+var server_name = "XminerSMP";
+var server_ip = "play.xminersmp.tk";
+var server_port = 25528;
 var ip_copied = "IP copied!";
 
 var firefly_count = 20;
 
-staff("OJWMine", "86fa328f-a99a-4a17-8250-5d333dd327fe", "Owner");
-staff("OJWMine", "86fa328f-a99a-4a17-8250-5d333dd327fe", "Developer");
+staff("Worthier Wind", "86fa328f-a99a-4a17-8250-5d333dd327fe", "Owner");
+staff("Binemok", "86fa328f-a99a-4a17-8250-5d333dd327fe", "Developer");
 staff("OJWMine", "86fa328f-a99a-4a17-8250-5d333dd327fe", "Admin");
 
 vote("Planet-Minecraft", "https://i.postimg.cc/9fmyrRkJ/Transparent-Planet-Minecraft-Logo.png", "https://planetminecraft.com");
