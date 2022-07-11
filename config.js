a4e495fbb061e5c65afc553c7e8f0b7c57b21e2b
binemok <94422811+binemok@users.noreply.github.com>
2022-07-11 14:03:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
  */
 
 var server_name = "XminerMC";
-var server_ip = "pe.xminermc.xyz";
-var server_port = 25582;
+var server_ip = "pc.xminermc.xyz";
+var server_port = 19132;
 var ip_copied = "IP copied!";
 
 var firefly_count = 20;
