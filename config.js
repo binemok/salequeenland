bf2fae0bb86d8a14b0426a84828dd17e312dbca4
binemok <94422811+binemok@users.noreply.github.com>
2023-05-09 14:46:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,13 @@
-/*!
- * Quara Theme by OllieJW (https://olliejw.me)
- * License - https://olliejw.me/tos
- */
 
-var server_name = "XminerMC";
-var server_ip = "pc.xminermc.xyz";
-var server_port = 19132;
+
+var server_name = "XminerSMP";
+var server_ip = "xminersmp.com";
+var server_port = 25565;
 var ip_copied = "IP copied!";
 
 var firefly_count = 20;
 
-staff("Worthier Wind", "86fa328f-a99a-4a17-8250-5d333dd327fe", "Owner");
-staff("Binemok", "8524ca6c-b2f9-4a7e-8321-8e51d7baf4b7", "Developer");
-staff("Manhboom", "a0b46b7f-9c7f-4930-bb8c-91b381b6110b", "Shareholder");
 
-vote("Planet-Minecraft", "https://i.postimg.cc/9fmyrRkJ/Transparent-Planet-Minecraft-Logo.png", "https://planetminecraft.com");
-vote("Minecraft Servers Org", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraftservers.org/");
-vote("Minecraft MP", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraft-mp.com/");
-vote("Top Minecraft Servers", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://topminecraftservers.org/");
+staff("Binemok", "8524ca6c-b2f9-4a7e-8321-8e51d7baf4b7", "Developer");
+
+
